318482ee3dc474572312dffa09df4b83d9ef2066
Neil McNaught <hello@neilmcnaught.dev>
2025-02-07 17:52:00 +0000

    desc updated

--- src/Pages/Home/components/index.jsx
+++ src/Pages/Home/components/index.jsx
@@ -7,15 +7,15 @@ export default function HomePage() {
 		<Wrapper page="Home">
 			<div className="intro">
 				<p>
-					Hey! I'm Neil. I'm a UK-based Freelance Software Developer, specialising in Web
-					Development. Day to day, I work with WordPress, PHP and Javascript. I could
-					probably develop you a WordPress website in my sleep! I'm also a big fan of
-					React which is what this website is built with.
+					Hey! I'm Neil. I'm the Lead Developer at a UK-based WordPress agency. Day to
+					day, I work with WordPress, PHP and Javascript. I could probably develop you a
+					WordPress website in my sleep! I'm also a big fan of React which is what this
+					website is built with.
 				</p>
 				<p>
-					Outside of work, you will still probably find me at my Mac tinkering with some
-					code. Right now I'm learning SwiftUI and I'm also working on a few personal
-					projects ~ watch this space!
+					Outside of work, I usually spend my time playing video games, watching TV or
+					spending time with my family. I'm also a big fan of football and support
+					Liverpool!.
 				</p>
 			</div>
 
